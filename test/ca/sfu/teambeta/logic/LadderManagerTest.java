e7acf7b47533ea97c9994a19476a894a533fb0fb
Kostiantyn Koval <kkoval@sfu.ca>
2016-06-12 14:57:28 -0700

    Remove not used code Fixes #4

--- test/ca/sfu/teambeta/logic/LadderManagerTest.java
+++ test/ca/sfu/teambeta/logic/LadderManagerTest.java
@@ -89,42 +89,6 @@ public class LadderManagerTest {
         Assert.assertEquals(expectedLadder, ladderManager.getLadder());
     }
 
-    /*
-    @Test
-    public void testApplyPenalties() {
-        LadderManager ladderManager = new LadderManager(testData());
-
-        ladderManager.setPenaltyToPair(0, "missing");
-        ladderManager.setPenaltyToPair(5, "late");
-        ladderManager.setPenaltyToPair(8, "missing");
-
-        //ladderManager.mergeActivePassive();
-        ladderManager.applyPenalties();
-
-        List<Pair> expectedLadder = Arrays.asList(
-            new Pair(new Player(3, "P3"), new Player(4, "P4"), false),
-            new Pair(new Player(5, "P5"), new Player(6, "P6"), false),
-            new Pair(new Player(7, "P7"), new Player(8, "P8"), false),
-            new Pair(new Player(9, "P9"), new Player(10, "P10"), false),
-            new Pair(new Player(13, "P13"), new Player(14, "P14"), false),
-            new Pair(new Player(15, "P15"), new Player(16, "P16"), false),
-            new Pair(new Player(11, "P11"), new Player(12, "P12"), false),
-            new Pair(new Player(19, "P19"), new Player(20, "P20"), false),
-            new Pair(new Player(1, "P1"), new Player(2, "P2"), false),
-            new Pair(new Player(17, "P17"), new Player(18, "P18"), false)
-        );
-
-        int position = 0;
-        for (Pair p : expectedLadder) {
-            position++;
-            p.setPosition(position);
-        }
-        List<Pair> ladder = ladderManager.getLadder();
-        //Assert.assertEquals(ladder, expectedLadder);
-
-    }
-    */
-
     @Test
     public void testAddNewPairAtIndex() {
         LadderManager ladderManager = new LadderManager(testData());
